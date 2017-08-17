cb61b732d2edf981515b22ea1ded483024853294
Robert Jones <robert.g.jones@gmail.com>
2017-08-17 23:28:50 +0100

    Change test expectionations and double creation
    The double is created with the Datamapper 'create' command so that it is
    given an :id and entered in to the test database. The :new command
    makes an Event instance but with id=nil.

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,13 +1,10 @@
 describe Event do
 
-  subject(:event) { described_class.new }
-  let(:event1) { described_class.new }
+  subject(:event) { described_class.create }
 
   describe 'has attributes of' do
     it 'id' do
-      event
-      event1 # the events must be called into the id block for the test
-      expect(event1.id).to eq(1)
+      expect(event.id).to eq(1)
     end
   end
 end
