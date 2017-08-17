37ec9395ae5f6343e1afc6078cd93be3cf611af0
Robert Jones <robert.g.jones@gmail.com>
2017-08-17 16:22:31 +0100

    Coordinates have a y attribute

--- lib/coordinate.rb
+++ lib/coordinate.rb
@@ -1,8 +1,9 @@
 class Coordinate
 
-  attr_reader :x
-  
-  def initialize(x)
+  attr_reader :x, :y
+
+  def initialize(x, y)
     @x = x
+    @y = y
   end
 end
--- spec/coordinate_spec.rb
+++ spec/coordinate_spec.rb
@@ -1,9 +1,12 @@
 require 'coordinate'
 
 describe Coordinate do
-  subject(:coord) { described_class.new(0)}
+  subject(:coord) { described_class.new(0, 1)}
 
   it "instantiates with an integer x co-ordinate" do
     expect(coord.x).to equal(0)
+  end
+  it "instantiateswith an integer y co-ordinate" do
+    expect(coord.y).to equal(1)
   end
 end
