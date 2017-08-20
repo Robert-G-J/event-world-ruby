7f70a427e18e8dd22c07db8bd5d57a252681b353
Robert Jones <robert.g.jones@gmail.com>
2017-08-20 01:19:46 +0100

    Remove Matrix tests and class

--- app/models/matrix.rb
+++
@@ -1,7 +0,0 @@
-require 'matrix'
-# alteration to Ruby Matrix class
-class Matrix
-  def []=(i, j, x)
-    @rows[i][j] = x
-  end
-end
--- spec/matrix_spec.rb
+++
@@ -1,7 +0,0 @@
-describe Matrix do
-  subject(:matrix) {described_class.zero(3)}
-  it "can assign values to elements" do
-    matrix[0, 0]= 'test assignment'
-    expect(matrix[0,0]).to eq 'test assignment'
-  end
-end
