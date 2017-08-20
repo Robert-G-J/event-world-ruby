31d1a37ff660f64060b92cbcc2d404cd5c5499de
Robert Jones <robert.g.jones@gmail.com>
2017-08-20 01:56:26 +0100

    Remove reference to deleted matrix.rb
    It clearly can't be required once it has been deleted. Remove line that
    breaks tests.

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,7 +5,6 @@ require_relative './models/event'
 require_relative './models/ticket'
 require_relative './models/coordinate'
 require_relative './models/grid'
-require_relative './models/matrix'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/event_world_#{ENV['RACK_ENV']}")
 
