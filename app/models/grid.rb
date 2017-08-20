134ae503fda9def866f10bc5d93aad8a7760cda6
Robert Jones <robert.g.jones@gmail.com>
2017-08-20 01:25:32 +0100

    Change method name for grid generator

--- app/models/grid.rb
+++ app/models/grid.rb
@@ -4,7 +4,7 @@ class Grid
     @area = {}
   end
 
-  def generate(x_min, x_max, y_min, y_max)
+  def generate_empty(x_min, x_max, y_min, y_max)
       (x_min..x_max).each { |x|
         (y_min..y_max).each { |y|
           @area[ [x,y] ] = nil
