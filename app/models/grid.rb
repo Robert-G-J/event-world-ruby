8539a1922f1d4f497f1133b8644b9854b85a35c4
Robert Jones <robert.g.jones@gmail.com>
2017-08-21 22:14:04 +0100

    Grid has coupling with Event removed
    Refactor Grid so that it can only generate an empty grid and has no
    knowledge of Event. Reword methods to reflect decoupling and clarify
    purpose.

--- app/models/grid.rb
+++ app/models/grid.rb
@@ -1,30 +1,38 @@
 # This allows a grid object to create a hash of coordinates as keys with nil values
 class Grid
 
-  attr_reader :area
+  attr_reader :area, :x_min, :x_max, :y_min, :y_max
 
-  def initialize
+  def initialize( x_min = -1,
+                  x_max = 1,
+                  y_min = -1,
+                  y_max = 1)
     @area = {}
+    @x_min, @x_max, @y_min, @y_max = x_min, x_max, y_min, y_max
+    @x_range = x_max - x_min
+    @y_range = y_max - y_min
   end
 
-  def generate_empty(x_min = -1, x_max = 1, y_min = -1, y_max = 1)
-    (x_min..x_max).each do |x|
-      (y_min..y_max).each do |y|
-        @area[[x, y]] = nil
+  def generate_empty
+    (self.x_min..self.x_max).each do |x|
+      (self.y_min..self.y_max).each do |y|
+        self.area[[x, y]] = nil
       end
     end
-    @area
+    self.area
   end
 
-  def assign_event(x, y, event)
-    self.area[[x,y]] = event
+  def assign_to_coordinate(x, y, object)
+    self.area[[x,y]] = object
   end
 
-  def man_distance(point_a, point_b)
-    raise "Invalid Coordinates" if (!point_a.is_a? Array) || (!point_b.is_a? Array)
-    (point_a.first - point_b.first).abs + (point_a.last - point_b.last).abs
+  def man_distance(start_point, end_point)
+    raise "Invalid Coordinates" if (!start_point.is_a? Array) || (!end_point.is_a? Array)
+    (start_point.first - end_point.first).abs + (start_point.last - end_point.last).abs
   end
 
   private
+ 
   attr_writer :area
+
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,18 +1,26 @@
 describe Grid do
-  subject(:grid) { described_class.new }
+  subject(:grid) { described_class.new(-1, 1, -1, 1) }
+  let(:grid_a) { spy("grid_a") }
   let(:event_a) { double(:event) }
   let(:event_b) { double(:event) }
+
   it 'can generate an array of arrays' do
-    expect(grid.generate_empty(-1, 1, -1, 1)).to eq(
+    expect(grid.generate_empty).to eq(
       { [-1, 1] => nil, [0, 1] => nil, [1, 1] => nil,
         [-1, 0] => nil, [0, 0] => nil, [1, 0] => nil,
         [-1,-1] => nil, [0,-1] => nil, [1,-1] => nil }
     )
   end
+
+  it 'can assign an event' do
+    grid.assign_event(1, 1, event_a )
+    expect(grid.area[[1,1]]).to eq(event_a)
+  end
+
   it 'can find Manhattan distance between points' do
     point_a = [-1, -1]
     point_b = [0, 0]
-    grid.generate_empty(-1, 1, -1, 1)
+    grid.generate_empty
     grid.assign_event(-1, -1, event_a)
     grid.assign_event(0, 0, event_b)
     expect(grid.man_distance(point_a, point_b)).to eq(2)
@@ -22,6 +30,7 @@ describe Grid do
     it 'if single integer given to #man_distance' do
       expect { grid.man_distance(2, [1,1]) }.to raise_error("Invalid Coordinates")
     end
+
     it 'if two integers given to #man_distance' do
       expect { grid.man_distance(2, 3) }.to raise_error("Invalid Coordinates")
     end
