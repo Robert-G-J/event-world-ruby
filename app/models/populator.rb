2a5fbbdeb89034aae2be8fa07858d4665f30dd53
Robert Jones <robert.g.jones@gmail.com>
2017-08-21 20:51:25 +0100

    The Populator can send a #prepare message to a preparable

--- app/models/populator.rb
+++ app/models/populator.rb
@@ -1,17 +1,8 @@
 # To randomly populate a grid with events
-require_relative 'grid'
-
 class Populator
 
-  attr_reader :grid
-
-  def initialize
-    @grid = Grid.new
+  def prepare(preparer)
+      preparer.prepare(self)
   end
 
-  def make_random_grid
-    self.grid.generate_empty
-    self.grid.assign_event(-1, 1, Event.new)
-    self.grid.area
-  end
 end
--- spec/populator_spec.rb
+++ spec/populator_spec.rb
@@ -1,18 +1,10 @@
 describe Populator do
   subject(:populator) { described_class.new }
-  event_a = Event.new
+  let(:preparable) { double(:preparable) }
 
-  grid_with_event = {
-     [-1, 1] => event_a, [0, 1] => nil, [1, 1] => nil,
-     [-1, 0] => nil, [0, 0] => nil, [1, 0] => nil,
-     [-1,-1] => nil, [0,-1] => nil, [1,-1] => nil }
-
-  it "makes grid, adds event" do
-    expect(populator.make_random_grid).to eq(grid_with_event)
+  it "sends prepare message to object" do
+    expect(preparable).to receive(:prepare)
+    populator.prepare(preparable)
   end
-end
 
-# expect make_random_grid should:
-# make a grid
-# (randomly) add events to the grid
-# store the grid
+end
