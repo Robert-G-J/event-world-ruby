d8716de18903602e30c4f2b66ef82226be94664a
Robert Jones <robert.g.jones@gmail.com>
2017-08-22 23:37:46 +0100

    GridPopulator #prepare_world method calls #prepare_grid
    The prepare_grid() function calls 'add_events()'

--- app/classes/grid_populator.rb
+++ app/classes/grid_populator.rb
@@ -1,7 +1,14 @@
 # To randomly populate a grid with events
 class GridPopulator
 
-  def prepare_world
-  	
+  def prepare_world(world)
+  	world.grids.each { |grid|
+  		prepare_grid(grid)
+  	}
   end
+
+  def prepare_grid(grid)
+  	add_events(grid)
+  end
+
 end
--- spec/grid_populator_spec.rb
+++ spec/grid_populator_spec.rb
@@ -1,5 +1,19 @@
 describe GridPopulator do
   subject(:grid_populator) { described_class.new }
+  let(:grid) { double(:grid) }
+  let(:world) { double(:world, :grids => [grid]) }
 
   it { is_expected.to respond_to(:prepare_world) }
+
+  it { is_expected.to respond_to(:prepare_grid).with(1).argument }
+
+  it 'prepare_grid' do
+    expect(grid_populator).to receive(:add_events)
+    grid_populator.prepare_grid(grid)
+  end
+
+  it 'prepare_world' do
+    expect(grid_populator).to receive(:prepare_grid)
+    grid_populator.prepare_world(world)
+  end
 end
