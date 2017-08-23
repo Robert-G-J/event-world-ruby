b216df99095d9a50727761277efb1f88fccd5124
Robert Jones <robert.g.jones@gmail.com>
2017-08-23 01:25:24 +0100

    Extract Grid dependency; inject after instantiation

--- app/classes/world.rb
+++ app/classes/world.rb
@@ -3,13 +3,17 @@ class World
 	attr_accessor :grids
 
 	def initialize
-		 @grids  = [Grid.new.extend(GridRandom)]
+		 @grids  = []
 	end
 
 	def populate(populators)
 		populators.each { |populator| 
 			populator.prepare_world(self) 
 		}
+	end
+
+	def add_grids(grid)
+		grids.push(grid)
 	end
 
 end
