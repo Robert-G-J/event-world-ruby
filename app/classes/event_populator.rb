78b784453eceb2cb968529614e6552b3ea513f33
Robert Jones <robert.g.jones@gmail.com>
2017-08-23 03:51:42 +0100

    EventPopulator can generate the random seed data
    Its code is tightly coupled as a result of it needing to automatically
    call the classes and not be handed them. Refactoring should definitely
    extract class references and instantiations from within the instance
    methods

--- app/classes/event_populator.rb
+++ app/classes/event_populator.rb
@@ -1,9 +1,33 @@
 class EventPopulator
 
-  def prepare_world(world)
-  	prepare_event
+	attr_reader :events
+
+  def initialize
+  	@events = []
   end
 
-  
+  def prepare_world(world)
+  	create_events
+  end
+
+# The following two methods have tightly coupled code to allow instantaneous creation of events and tickets. It makes me uncomfortable...
+  def create_events
+  	rand(1..400).times do 
+  		event = Event.new
+  		self.events.push(event)
+  	end
+  	self.events.each do |event|
+  		event.add_tickets(generate_tickets)
+  	end
+  end
+
+  def generate_tickets
+    tickets = []
+  	rand(1...5000).times do 
+  		ticket = Ticket.new(rand(20..50))
+  		tickets << ticket
+  	end
+  	tickets
+  end
 
 end
--- spec/event_populator_spec.rb
+++ spec/event_populator_spec.rb
@@ -2,11 +2,31 @@ describe EventPopulator do
 	subject(:event_populator) { described_class.new }
   let(:grid) { double(:grid, :random_range_product => 1, :random_x => 1, :random_y => 1) }
   let(:world) { double(:world, :grids => [grid]) }
+  let(:ticket) { double(:ticket) }
+  let(:event) { double(:event, :generate_tickets => true, :add_tickets => [ticket] ) }
 
 	it { is_expected.to respond_to :prepare_world }
+	it { is_expected.to respond_to :create_events }
+  it { is_expected.to respond_to :generate_tickets }
 
 	it 'prepare_world calls prepare_event' do
-    expect(event_populator).to receive(:prepare_event)
+    expect(event_populator).to receive(:create_events)
     event_populator.prepare_world(world)
   end
+
+  describe '#create_events' do
+    it 'pushes events to memory ' do
+      expect(event_populator.events).to receive(:push).at_least(:once)
+      event_populator.create_events
+    end
+  end
+  
+  describe '#generate_tickets' do
+    it 'calls concat on tickets' do
+      allow(event_populator).to receive(:rand) { 1 }
+      expect(event_populator.generate_tickets).to include(Ticket)
+    end
+  end
+
+
 end
