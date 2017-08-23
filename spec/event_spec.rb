b8e7a1683088be367e8a96eda266a870afcf30cf
Robert Jones <robert.g.jones@gmail.com>
2017-08-23 03:56:31 +0100

    Refactor event spec to account for change from DB to memory

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,10 +1,24 @@
 describe Event do
 
-  subject(:event) { described_class.create }
+  subject(:event) { described_class.new }
+  let(:ticket) { double(:ticket) }
 
   describe 'has attributes of' do
-    it 'id' do
-      expect(event.id).to eq(1)
-    end
+    it { is_expected.to respond_to :id }
   end
+
+  describe 'has behaviours' do
+  	it { is_expected.to respond_to :add_ticket }
+
+  	it 'sends ticket to tickets array' do
+  		expect(event.tickets).to receive(:<<)
+  		event.add_ticket(ticket)
+  	end
+
+  	it 'calls concat on tickets' do
+  		expect(event.tickets).to receive(:concat)
+  		event.add_tickets([])
+  	end
+  end
+
 end
